da9b590248c15f89347d79418395692196e845c2
alihinnawe <hinnaweali@gmail.com>
2024-01-25 15:02:32 +0100

    Update cv

--- src/PortfolioContainer/AboutMe/AboutMe.js
+++ src/PortfolioContainer/AboutMe/AboutMe.js
@@ -1,4 +1,3 @@
-import React, { useEffect } from "react";
 import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
 import ScrollService from "../../utilities/ScrollService";
 import Animations from "../../utilities/Animations";
